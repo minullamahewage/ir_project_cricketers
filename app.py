ae194043d58d8d4ab152a6b7f92d39168a71cd6a
Minul Lamahewage <minullamahewage@gmail.com>
2021-10-26 20:12:57 +0530

    added index.html for search and displaying results. Fixed app.py and search.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask
-import pickle
 from flask import jsonify, request
-from elasticsearch import Elasticsearch, helpers
-import requests
+from elasticsearch import Elasticsearch
 from flask import flash, render_template, request, redirect, jsonify
 
 es = Elasticsearch([{'host': '127.0.0.1', 'port':9200}])
@@ -12,31 +10,17 @@ app = Flask(__name__)
 @app.route('/', methods=['GET','POST'])
 def search_actor():
     if request.method == 'POST':
-        if request.form['query']:
-            query = request.form['query']
-            # print(query)
+        if request.form['search_query']:
+            query = request.form['search_query']
+            print(query)
         else:
             query = ''
-        results = search(query)
+        cricketers = search(query)
 
-
-    # keyword = request.form['keyword']
-
-    # query_body = {
-    #     "query": {
-    #         "match": {
-    #             "name_si": keyword
-    #         }
-    #     }
-    # }
-    #
-    # res = es.search(index="index-actors", body=query_body)
-        return jsonify(results)
-        # return render_template('index.html', actors= actors, names = names, real_names = real_names, birthdays = birthdays, diedes = diedes, addresses = addresses)
+        return render_template('index.html', cricketers= cricketers)
     else:
-        return {"task": "profit!"}
-        # return render_template('index.html', actors= '', names = '', real_names = '', birthdays = '', diedes = '', addresses = '')
+        return render_template('index.html', cricketers='')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- search.py
+++ search.py
@@ -4,7 +4,7 @@ import re
 
 es = Elasticsearch([{'host': '127.0.0.1', 'port':9200}])
 
-def keyword_search(query):
+def search(query):
     print(query)
     results = es.search(index='index-cricketers', body={
         "size": 10,
@@ -20,42 +20,14 @@ def keyword_search(query):
         },
     })
 
-    print(results)
-    # Full_Name, Batting_Style, Player_Bio  = post_processing(results)
-    return results
+    return post_processing(results)
 
 def post_processing(results):
-    actors = []
+    cricketers = []
     for i in range(len(results['hits']['hits'])):
-        # lyrics = json.dumps(results['hits']['hits'][i]['_source']["name"], ensure_ascii=False)
-        # lyrics = lyrics.replace('"', '')
-        # lyrics = lyrics.replace("'", '')
-        # lyrics = lyrics.replace('\\', '')
-        # lyrics = lyrics.replace('t', '')
-        # lyrics = lyrics.replace('\xa0', '')
-        # lyrics = "<br>".join(lyrics.split("n"))
-        # lyrics = re.sub(r'(<br> )+', r'\1', lyrics)
-        # j = 0
-        # while True:
-        #     if lyrics[j] == '<' or lyrics[j] == '>' or lyrics[j] == 'b' or lyrics[j] == 'r' or lyrics[j] == ' ':
-        #         j += 1
-        #     else:
-        #         break
-        # lyrics = lyrics[j:]
-        # results['hits']['hits'][i]['_source']["song_lyrics"] = lyrics
-        actors.append(results['hits']['hits'][i]['_source'])
-    aggregations = results['aggregations']
-    names = aggregations['name']['buckets']
-    real_names = aggregations['real_name']['buckets']
-    birthdays = aggregations['birthday']['buckets']
-    diedes = aggregations['died']['buckets']
-    addresses = aggregations['address']['buckets']
+        cricketers.append(results['hits']['hits'][i]['_source'])
 
-    return actors,names, real_names, birthdays, diedes, addresses
-
-def search(query):
-    results = keyword_search(query)
-    return results
+    return cricketers
 
 # if __name__ == "__main__":
 #     print(search("පිතිකරු"))
